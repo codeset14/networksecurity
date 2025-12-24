9064f7673f05dd2fba1d96f7a023952caff9bcb9
Viraj Sawant <sawant1418@gmail.com>
2025-12-24 09:34:19 +0530

    k

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ if __name__=='__main__':
 
         logging.info("Model Training sstared")
         model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        logging.info("Model Training config complete")
         model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
         model_trainer_artifact=model_trainer.initiate_model_trainer()
 
